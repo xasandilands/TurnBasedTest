76f1cf77d28103bb7aebd615da254c2ae2ded5b9
Xavier <njsandilands@gmail.com>
2025-07-14 16:20:14 -0500

    Prefabs + unit script
    created player and enemy prefabs and made simple unit scirpt

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string UnitName;
+    public int UnitLvl;
+
+    public int Damage;
+
+    public int MaxHealth;
+    public int CurHealth;//current health
+}
